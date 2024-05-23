0f631779478b22bc5ee5a0a24bedfd33f8e623bd
sandhyaba <86476805+sandhyaba@users.noreply.github.com>
2024-05-23 11:44:22 +0530

    Update db connection

--- utils/db.js
+++ utils/db.js
@@ -1,4 +1,13 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:27017/Edusity')
-  .then(() => console.log('Connected!'));
+// mongoose.connect('mongodb://127.0.0.1:27017/Edusity')
+//   .then(() => console.log('Connected!'));
+const uri = "mongodb+srv://baisa007:baisa007@cluster0.xp1qy5z.mongodb.net/"
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => {
+  console.log('Connected to MongoDB');
+}).catch(err => {
+  console.error('Error connecting to MongoDB', err);
+});

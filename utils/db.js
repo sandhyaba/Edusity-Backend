b31793709d5fa10e6dae3ff0ff5be93a81d42dc2
sandhyaba <86476805+sandhyaba@users.noreply.github.com>
2024-05-24 11:15:10 +0530

    Update db connecction

--- utils/db.js
+++ utils/db.js
@@ -5,9 +5,12 @@ const mongoose = require('mongoose');
 const uri = "mongodb+srv://baisa007:baisa007@cluster0.bc5uahx.mongodb.net/"
 mongoose.connect(uri, {
   useNewUrlParser: true,
-  useUnifiedTopology: true
+  useUnifiedTopology: true,
+  serverSelectionTimeoutMS: 5000,  // Adjust the timeout value as needed
+  socketTimeoutMS: 45000,          // Optional: Ensure socket timeout is appropriate
+  // More options if needed
 }).then(() => {
-  console.log('Connected to MongoDB');
-}).catch(err => {
-  console.error('Error connecting to MongoDB', err);
+  console.log('MongoDB connected');
+}).catch((err) => {
+  console.error('Error connecting to MongoDB:', err);
 });
